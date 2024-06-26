2186dd7bbe83c13dbb98a52ebecc53a7b7fd895b
Paulo Felipe Souza <alparakleto@gmail.com>
2024-06-26 20:43:04 -0300

    2001 a space odyssey

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+## [Unreleased]
+
+## [0.1.0] - 2024-06-26
+
+- Initial release
---
+++ lib/inss_calculator.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "inss_calculator/version"
+
+module InssCalculator
+  class Error < StandardError; end
+  # Your code goes here...
+end
