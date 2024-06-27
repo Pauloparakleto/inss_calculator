224f4f82ffd33c03d5fdd1fdc63813b211c3fa99
Paulo Felipe Souza <alparakleto@gmail.com>
2024-06-26 21:56:01 -0300

    feat lib/inss_calculator.rb: add feature

--- lib/inss_calculator.rb
+++ lib/inss_calculator.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 require_relative "inss_calculator/version"
+require_relative 'inss_calculator/discount_calculator_base'
+require_relative 'inss_calculator/discount_previdence_calculator'
+require_relative 'inss_calculator/first_discount_calculator'
+require_relative 'inss_calculator/second_discount_calculator'
+require_relative 'inss_calculator/third_discount_calculator'
+require_relative 'inss_calculator/fourth_discount_calculator'
 
 module InssCalculator
   class Error < StandardError; end
