07d08dd7570581103db942db2b5f7a07f28d6683
Paulo Felipe Souza <alparakleto@gmail.com>
2024-07-03 18:53:57 -0300

    refactor third_discount_calculator.rb
    make it dependent on InssCalculator

--- lib/inss_calculator/third_discount_calculator.rb
+++ lib/inss_calculator/third_discount_calculator.rb
@@ -5,16 +5,31 @@ module InssCalculator
   # In case the salary is beyond this limit, it will use its quotation apply inside the range salary limit
   class ThirdDiscountCalculator < DiscountCalculatorBase
     QUOTATION = 0.12
-    SALARY_BASE = 2666.68
-    SALARY_LIMIT = 4000.03
-    FULL_CONTRIBUTION = ((SALARY_LIMIT - SALARY_BASE) * QUOTATION).truncate(2)
 
     def contribution
-      return NO_CONTRIBUTION if salary <= SALARY_BASE
+      return NO_CONTRIBUTION if salary < salary_base
 
-      return FULL_CONTRIBUTION if salary > SALARY_LIMIT
+      return full_contribution if salary > salary_limit
 
-      ((salary - SALARY_BASE) * QUOTATION).truncate(2)
+      calculate_contribution
+    end
+
+    private
+
+    def calculate_contribution
+      ((salary - salary_base) * QUOTATION).truncate(2)
+    end
+
+    def full_contribution
+      ((salary_limit - salary_base) * QUOTATION).truncate(2)
+    end
+
+    def salary_limit
+      InssCalculator::THIRD_SALARY_LIMIT
+    end
+
+    def salary_base
+      InssCalculator::THIRD_SALARY_BASE
     end
   end
 end
