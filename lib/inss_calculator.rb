b6124a531843a794ba8e27ad44e5022380adab1b
paulo-felipe <alparakleto@gmail.com>
2024-07-16 22:49:37 -0300

    feat calculate fifth contribution

--- lib/inss_calculator.rb
+++ lib/inss_calculator.rb
@@ -7,6 +7,7 @@ require_relative 'inss_calculator/first_discount_calculator'
 require_relative 'inss_calculator/second_discount_calculator'
 require_relative 'inss_calculator/third_discount_calculator'
 require_relative 'inss_calculator/fourth_discount_calculator'
+require_relative 'inss_calculator/fifth_discount_calculator'
 
 module InssCalculator
   class Error < StandardError; end
@@ -22,4 +23,15 @@ module InssCalculator
 
   FOURTH_SALARY_BASE = 4000.04
   FOURTH_SALARY_LIMIT = 7786.02
+
+  FIFTH_SALARY_BASE = 7786.03
+  FIFTH_SALARY_LIMIT = 13333.48
+
+  SIXTH_SALARY_BASE = 13333.49
+  SIXTH_SALARY_LIMIT = 26666.94
+
+  SEVENTH_SALARY_BASE = 26666.95
+  SEVENTH_SALARY_LIMIT = 52000.54
+
+  EIGTH_SALARY_BASE = 52000.55
 end
--- spec/inss_calculator_spec.rb
+++ spec/inss_calculator_spec.rb
@@ -46,10 +46,52 @@ RSpec.describe InssCalculator do
       expect(described_class::FOURTH_SALARY_BASE).to eq(4000.04)
     end
   end
-  
+ 
   describe 'FOURTH_SALARY_LIMIT' do
     it 'is equal to 7786.02' do
       expect(described_class::FOURTH_SALARY_LIMIT).to eq(7786.02)
+    end
+  end
+ 
+  describe 'FIFTH_SALARY_BASE' do
+    it 'is equal to 7786.03' do
+      expect(described_class::FIFTH_SALARY_BASE).to eq(7786.03)
+    end
+  end
+ 
+  describe 'FIFTH_SALARY_LIMIT' do
+    it 'is equal to 13333.48' do
+      expect(described_class::FIFTH_SALARY_LIMIT).to eq(13333.48)
+    end
+  end
+ 
+  describe 'SIXTH_SALARY_BASE' do
+    it 'is equal to 13333.49' do
+      expect(described_class::SIXTH_SALARY_BASE).to eq(13333.49)
+    end
+  end
+ 
+  describe 'SIXTH_SALARY_LIMIT' do
+    it 'is equal to 26666.94' do
+      expect(described_class::SIXTH_SALARY_LIMIT).to eq(26666.94)
+    end
+  end
+ 
+  describe 'SEVENTH_SALARY_BASE' do
+    it 'is equal to 26666.95' do
+      expect(described_class::SEVENTH_SALARY_BASE).to eq(26666.95)
+    end
+  end
+ 
+  describe 'SEVENTH_SALARY_LIMIT' do
+    it 'is equal to 52000.54' do
+      expect(described_class::SEVENTH_SALARY_LIMIT).to eq(52000.54)
+    end
+  end
+ 
+  describe 'EIGTH_SALARY_BASE' do
+    it 'is equal to 52000.55' do
+      expect(described_class::SEVENTH_SALARY_BASE).to eq(26666.95)
     end
   end
 end
