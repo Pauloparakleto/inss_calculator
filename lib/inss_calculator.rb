0d4ad43d712acd748db5742cd788601b274a31ca
Paulo Felipe Souza <alparakleto@gmail.com>
2024-08-06 11:20:24 -0300

    feat public inss calculator

--- lib/inss_calculator.rb
+++ lib/inss_calculator.rb
@@ -3,6 +3,7 @@
 require_relative 'inss_calculator/version'
 require_relative 'inss_calculator/discount_calculator_base'
 require_relative 'inss_calculator/discount_previdence_calculator'
+require_relative 'inss_calculator/public_inss_calculator'
 require_relative 'inss_calculator/first_discount_calculator'
 require_relative 'inss_calculator/second_discount_calculator'
 require_relative 'inss_calculator/third_discount_calculator'
