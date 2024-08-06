b040061c4be2841e9836a1e969b4414730efe9b5
Paulo Felipe Souza <alparakleto@gmail.com>
2024-08-06 11:31:00 -0300

    refactor public inss calculator
    consider also parent contribution sum on the public contribution

--- lib/inss_calculator/public_inss_calculator.rb
+++ lib/inss_calculator/public_inss_calculator.rb
@@ -4,27 +4,18 @@ module InssCalculator
   # Based on this insecure protocoll site https://www.coalize.com.br/calculadora-de-inss
   # Check also the official law https://www.in.gov.br/en/web/dou/-/portaria-interministerial-mps/mf-n-2-de-11-de-janeiro-de-2024-537035232
   class PublicInssCalculator < InssCalculator::DiscountPrevidenceCalculator
-    attr_reader :salary
-
-    def initialize(salary)
-      @salary = salary.to_f
+    def contribution
+      super + public_contribution
     end
 
-    def contribution
-      [
-        FirstDiscountCalculator.new(salary).contribution,
-        SecondDiscountCalculator.new(salary).contribution,
-        ThirdDiscountCalculator.new(salary).contribution,
-        FourthDiscountCalculator.new(salary).contribution,
-        FifthDiscountCalculator.new(salary).contribution,
+    private
+
+    def public_contribution
+      [ FifthDiscountCalculator.new(salary).contribution,
         SixthDiscountCalculator.new(salary).contribution,
         SeventhDiscountCalculator.new(salary).contribution,
         EigthDiscountCalculator.new(salary).contribution
       ].reduce(:+).round(2)
-    end
-
-    def net_salary
-      (salary - contribution).round(2)
     end
   end
 end
