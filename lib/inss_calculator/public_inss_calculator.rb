1c8330de8e0d43a67a1e5365ec863e7618b26cc9
Paulo Felipe Souza <alparakleto@gmail.com>
2024-08-06 11:32:27 -0300

    add comment to public calculator

--- lib/inss_calculator/public_inss_calculator.rb
+++ lib/inss_calculator/public_inss_calculator.rb
@@ -3,6 +3,8 @@
 module InssCalculator
   # Based on this insecure protocoll site https://www.coalize.com.br/calculadora-de-inss
   # Check also the official law https://www.in.gov.br/en/web/dou/-/portaria-interministerial-mps/mf-n-2-de-11-de-janeiro-de-2024-537035232
+  # The public contribution is an extension of the private contribution.
+  # They have the same rules and quotes percentagen until the fourth salary level.
   class PublicInssCalculator < InssCalculator::DiscountPrevidenceCalculator
     def contribution
       super + public_contribution
