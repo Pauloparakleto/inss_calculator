95c3878de439aa115be8c425a55da666eede7aff
Paulo Felipe Souza <alparakleto@gmail.com>
2025-01-29 20:07:20 -0300

    add 2025 firs discount calculator

---
+++ spec/inss_calculator/first_discount_calculator_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+include InssCalculator
+
+RSpec.describe FirstDiscountCalculator do
+  it 'is true' do
+    expect(described_class.new(3000)).to be_truthy
+  end
+
+  context 'when salary is 0' do
+    it 'is 0' do
+      expect(described_class.new('0').contribution).to eq(0)
+    end
+  end
+
+  context 'when salary is 1412.0' do
+    it 'is 105.9' do
+      expect(described_class.new('1412.0').contribution).to eq(105.9)
+    end
+  end
+
+  context 'when salary is 1100.0' do
+    it 'is 82.5' do
+      expect(described_class.new('1100.0').contribution).to eq(82.5)
+    end
+  end
+
+  context 'when salary is above 3412.01' do
+    it 'is 113.85' do
+      expect(described_class.new('3412.01').contribution).to eq(113.85)
+    end
+  end
+end
